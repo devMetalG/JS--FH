1f7bd7c72eaffa838eb0dcefd1776feb100afda5
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-28 13:41:48 -0600

    add: consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,7 +1,16 @@
 // console.log('hola mundo JS')
 
 let a = 10
-var b = 10
-const c = 10
+var b = 20
+const c = 30
+const d = 'Hola'
+const e = 'Spiderman'
 
-console.log(a)
+const saludo = `${d} ${e}`
+
+console.log('%c Mis variables', 'color:lightgreen; font-weight: bold')
+console.log({a})
+console.log({b})
+console.log({c})
+
+console.table({a, b, c})
