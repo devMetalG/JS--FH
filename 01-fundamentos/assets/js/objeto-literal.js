42f79b58b0872a8bc86e05381550a2651edcfa35
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-28 22:00:11 -0600

    add: objetos litereales mas detalles

--- 01-fundamentos/assets/js/objeto-literal.js
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -24,3 +24,22 @@ console.log(`Ultimo traje: ${personaje.trajes[personaje.trajes.length - 1]}.`)
 const x = 'vivo'
 
 console.log('Esta vivo?:', personaje[x]? 'Si' : 'No')
+
+// Mas detalles
+
+delete personaje.edad
+console.log(personaje)
+
+personaje.casado = false
+
+const entriesPares = Object.entries(personaje)
+console.log(entriesPares)
+
+Object.freeze(personaje)
+personaje.dinero = 10
+console.log(personaje)
+
+const propiedades = Object.getOwnPropertyNames(personaje)
+const values = Object.values(personaje)
+console.log(propiedades)
+console.log(values)
