414bac1696e936be21ef4eb874930ee9deb2e951
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-28 22:43:25 -0600

    add: retorno de funciones

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -15,3 +15,23 @@ const saludoFlecha = name => {
 }
 
 saludoFlecha('Memo3')
+
+function suma(a, b) {
+  return a + b 
+}
+const resultado = suma(10, 5) 
+console.log(resultado)
+
+const sumar = (a, b) =>  a + b
+
+
+const resultado2 = sumar(2, 4)
+console.log(resultado2)
+
+function getRandom () {
+  return Math.random()
+}
+const nRandom =  () => Math.random()
+
+console.log(getRandom())
+console.log(nRandom())
