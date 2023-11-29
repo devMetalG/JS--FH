92f34d053dd8742f9f531cf412570d9af0bac9d9
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-29 17:08:47 -0600

    add: asignaciones

--- 01-fundamentos/assets/js/logica-booleana.js
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -31,3 +31,16 @@ console.log(false || false)
 console.log('=============')
 console.log(regresaFalse() || regresaTrue())
 console.log(regresaTrue() || regresaFalse())
+
+console.warn('Asignaciones')
+
+const soyNull = null
+const soyUndefined = undefined
+const soyFalse = false
+
+const a1 = true && 'Hola Mundo'
+const a2 = 'Hola' && 'Mundo'
+const a3 = soyFalse || 'Ya no soy falso'
+const a4 = soyFalse || soyUndefined || soyNull || 'Ya no soy falso'
+const a5 = soyFalse || soyUndefined || regresaTrue() || 'Ya no soy falso'
+console.log({a1, a2, a3, a4, a5})
