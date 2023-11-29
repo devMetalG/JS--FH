57d9ddeeddf538de1df18be07941696adc7813e1
Guillermo Franco <fra_borjon@hotmail.com>
2023-11-28 18:55:18 -0600

    add: palabras-reservadas.js

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,14 @@
+let π = Math.PI
+console.log(π)
+
+// let for = 'a' // error: palabra reservada
+
+// lowerCamelCase
+let _nombre = 'Memo'
+let $nombre = 'Joel'
+
+// UpperCamelCase
+
+class Game {
+  
+}
