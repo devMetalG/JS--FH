7e238c98b793ab07f6d8406666dda94e0a038be5
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-09 17:11:49 -0600

    add: metodos en clases

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -8,9 +8,20 @@ class Persona {
     this.codigo = codigo
     this.frase = frase
   }
+  quienSoy(){
+    console.log(`Soy ${this.nombre} y mi identidad es: ${this.codigo}`)
+  }
+  miFrase(){
+    this.quienSoy()
+    console.log(`${this.codigo} dice: ${this.frase}`)
+  }
 }
 
-const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman')
+const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman')
 const ironman = new Persona('Tony Stark', 'IronMan', 'Yo soy Ironman')
 console.log(spiderman)
 console.log(ironman)
+
+
+spiderman.miFrase()
+ironman.miFrase()
