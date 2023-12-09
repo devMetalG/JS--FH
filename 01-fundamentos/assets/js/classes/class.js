0787c408f8a632791cf56b50df072b07aeab8b12
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-09 17:50:57 -0600

    add: sets y gets

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -2,12 +2,22 @@ class Persona {
   nombre = ''
   codigo = ''
   frase = ''
+  comida = ''
 
   constructor(nombre = 'Sin nombre', codigo= 'Sin codigo', frase = 'Sin frase'){
     this.nombre = nombre
     this.codigo = codigo
     this.frase = frase
   }
+
+  set setComidaFavorita(comida){
+    this.comida = comida.toUpperCase()
+  }
+
+  get getComidaFavorita(){
+    return `La comida favorita de: ${this.nombre} es: ${this.comida}`
+  }
+
   quienSoy(){
     console.log(`Soy ${this.nombre} y mi identidad es: ${this.codigo}`)
   }
@@ -19,9 +29,11 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman')
 const ironman = new Persona('Tony Stark', 'IronMan', 'Yo soy Ironman')
-console.log(spiderman)
 console.log(ironman)
 
 
 spiderman.miFrase()
-ironman.miFrase()
+
+spiderman.setComidaFavorita = 'El pay de cereza'
+console.log(spiderman.getComidaFavorita)
+console.log(spiderman)
