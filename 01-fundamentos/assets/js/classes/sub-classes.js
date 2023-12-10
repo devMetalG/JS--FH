1b7d83ca90a30a84c5e46acf99e0f68c96bd5bac
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-09 20:28:40 -0600

    add: extends classes

---
+++ 01-fundamentos/assets/js/classes/sub-classes.js
@@ -0,0 +1,60 @@
+class Persona {
+
+  static _conteo = 0
+  static get conteo(){
+    return Persona._conteo + ' Instancias'
+  }
+
+  static mensaje (){
+    console.log(this.nombre)
+    console.log('Hola a todos metodo static')
+  }
+
+  nombre = ''
+  codigo = ''
+  frase = ''
+  comida = ''
+
+  constructor(nombre = 'Sin nombre', codigo= 'Sin codigo', frase = 'Sin frase'){
+    this.nombre = nombre
+    this.codigo = codigo
+    this.frase = frase
+
+    Persona._conteo ++
+  }
+
+  set setComidaFavorita(comida){
+    this.comida = comida.toUpperCase()
+  }
+
+  get getComidaFavorita(){
+    return `La comida favorita de: ${this.nombre} es: ${this.comida}`
+  }
+
+  quienSoy(){
+    console.log(`Soy ${this.nombre} y mi identidad es: ${this.codigo}`)
+  }
+  miFrase(){
+    this.quienSoy()
+    console.log(`${this.codigo} dice: ${this.frase}`)
+  }
+}
+
+class Heroe extends Persona {
+  
+  clan = 'Sin Clan'
+  constructor(nombre, codigo, frase){
+    super(nombre, codigo, frase)
+    this.clan = 'Los Avengers'
+  }
+  quienSoy(){
+    console.log(`Soy ${this.nombre}, ${this.clan}`)
+    super.quienSoy()
+  }
+}
+
+const spiderman = new Heroe('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman')
+// const spiderman = new Heroe()
+
+console.log(spiderman)
+spiderman.quienSoy()
