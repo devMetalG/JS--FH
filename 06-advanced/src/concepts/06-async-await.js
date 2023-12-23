d47ebbbdcde9180e7ce9a37811b906ec9d67e499
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-22 21:54:50 -0600

    add: errores en async await (try-catch)

--- 06-advanced/src/concepts/06-async-await.js
+++ 06-advanced/src/concepts/06-async-await.js
@@ -8,10 +8,14 @@ export const asyncAwaitComponent = async element => {
   const id1 = '5d86371fd55e2e2a30fe1ccb'
   const id2 = '5d86371f2343e37870b91ef1'
 
-  const hero1 = await findHero(id1)
-  const hero2 = await findHero(id2)
-
-  element.innerHTML = `${hero1.name} / ${hero2.name}`
+  try {
+    const hero1 = await findHero(id1)
+    const hero2 = await findHero(id2)
+  
+    element.innerHTML = `${hero1.name} / ${hero2.name}`
+  } catch (error) {
+    element.innerHTML = error
+  }
 }
 
 const findHero = async id => {
