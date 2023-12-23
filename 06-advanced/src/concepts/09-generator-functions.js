9e530f3a3b84926d0dd98d3fb8d680095571cfce
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-23 00:57:34 -0600

    add: funciones generadoras ejemplo

--- 06-advanced/src/concepts/09-generator-functions.js
+++ 06-advanced/src/concepts/09-generator-functions.js
@@ -3,13 +3,25 @@
  * @param {HTMLDivElement} element 
  */
 export const generatorFunctionsComponent = (element) => {
-  const myGenerator = myFirstGeneratorFunction()
+  // const myGenerator = myFirstGeneratorFunction()
+  // console.log(myGenerator.next())
+  const genID = idGenerator()
+  const button = document.createElement('button')
+  button.textContent = 'Click me!'
+  element.appendChild(button)
+  const renderButton = () => {
+    const { value } = genID.next()
+    button.innerText = `Click: ${value}`
+  }
 
-  console.log(myGenerator.next())
-  console.log(myGenerator.next())
-  console.log(myGenerator.next())
-  console.log(myGenerator.next())
-  console.log(myGenerator.next())
+  button.addEventListener('click', renderButton)
+}
+
+function* idGenerator() {
+  let currentID = 0
+  while (true) {
+    yield ++currentID
+  }
 }
 
 function* myFirstGeneratorFunction(){
