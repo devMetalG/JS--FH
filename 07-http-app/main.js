2c5ac60a1ccd28164fb706fbdb421d293fc61c3d
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-23 15:39:37 -0600

    add: breakingbad app

--- 07-http-app/main.js
+++ 07-http-app/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { BreakingBadApp } from './src/breakingbad/breakingbad-app'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -12,8 +13,9 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1 id="app-title">Hello Vite!</h1>
     <div class="card">
-      
+
     </div>
   </div>
 `
-
+const element = document.querySelector('.card')
+BreakingBadApp(element)
---
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingBadApp = element => {
+  document.querySelector('#app-title').innerHTML = 'BreakingBad App'
+  element.innerHTML = 'Loading...'
+}
