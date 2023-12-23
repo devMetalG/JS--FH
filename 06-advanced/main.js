bbc28f415d5f33a95fd276ad8e302ac83411e33e
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-23 01:13:54 -0600

    add: generator async

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -10,6 +10,7 @@ import { asyncAwaitComponent } from './src/concepts/06-async-await'
 import { asyncAwait2Component } from './src/concepts/07-async-await'
 import { forAwaitComponent } from './src/concepts/08-for-await'
 import { generatorFunctionsComponent } from './src/concepts/09-generator-functions'
+import { generatorAsyncComponent } from './src/concepts/10-generator-async'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -33,4 +34,5 @@ const element = document.querySelector('.card')
 // asyncAwaitComponent(element)
 // asyncAwait2Component(element)
 // forAwaitComponent(element)
-generatorFunctionsComponent(element)
+// generatorFunctionsComponent(element)
+generatorAsyncComponent(element)
