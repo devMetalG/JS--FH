fc4ae076a8a3bf2acd9a06c5a219676acc8f1a6e
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-26 21:17:20 -0600

    add: caso de uso crear usuario

---
+++ 08-CRUD-App/src/users/use-cases/save-user.js
@@ -0,0 +1,36 @@
+import {User} from '../models/user'
+/**
+ * 
+ * @param {Like<User>} userLike 
+ */
+export const saveUser = async (userLike) => {
+
+  const user = new User(userLike)
+  //TODO: Falta mapper
+
+  if (user.id) {
+    throw 'Not implemented.'
+  }
+
+  const updatedUser = await createUser(user)
+  return updatedUser
+}
+
+/**
+ * 
+ * @param {Like<User>}
+ */
+const createUser = async (user) => {
+  const url = `${import.meta.env.VITE_BASE_URL}/users`
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  const newUser = await response.json()
+
+  return newUser
+}
