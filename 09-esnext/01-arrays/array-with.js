b90340ca535654cf1c5aec2ed5561637a50688f1
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-27 00:13:32 -0600

    add: structuredClone

--- 09-esnext/01-arrays/array-with.js
+++ 09-esnext/01-arrays/array-with.js
@@ -1,1 +1,28 @@
-console.log('hola mundo')
+const superHeroes = [
+  {
+    id: 1,
+    name:'batman'
+  },
+  {
+    id: 2,
+    name:'Superman'
+  },
+  {
+    id: 3,
+    name:'Aquaman'
+  },
+  {
+    id: 4,
+    name:'Wonderwoman'
+  },
+]
+
+// Hacer uso del spread operator si dentro del arreglo hay primitivos
+// const superHeroesCopy = [...superHeroes]
+
+// Structured Clone
+const superHeroesCopy = structuredClone(superHeroes)
+
+superHeroesCopy[3].name = 'Flash'
+console.table(superHeroes)
+console.table(superHeroesCopy)
